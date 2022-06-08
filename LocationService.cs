0911e345b31bbec304e01598b73140a2ae26cd41
Esther Tan <EstherpgTan@gmail.com>
2022-06-09 00:17:03 +1000

    Updated

---
+++ LocationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RobotInvasion.Services
+{
+	public class LocationService
+	{
+		private readonly HttpClient _httpClient;
+
+		public LocationService(HttpClient client)
+        {
+			client.DefaultRequestHeaders.Add("User", "c# app");
+			_httpClient = client;
+		}
+
+		public async Task<string> GetNearestBodyOfWater(Location location)
+		{
+			HttpClient client = new HttpClient();
+			HttpResponseMessage y = await client.GetAsync("https://nominatim.openstreetmap.org/reverse?format=json&lat=-32&lon=151.2082848"<param>)
+				return await y.Content.ReadAsStringAsync();
+		}
+	}
+}
+
---
+++ RobotController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RobotInvasion.Models;
+
+namespace RobotInvasion.Controllers;
+
+public class RobotController : Controller
+{
+    private readonly ILogger<RobotController> _logger;
+    private readonly LocationService _service;
+
+    public RobotController(LocationService service, ILogger<RobotController> logger)
+    {
+        _service = service;
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "RobotLocated")]
+    
+    public string Get()
+    {
+        return "test";
+    }
+    [HttpPost(Name = "RobotLocated")]
+    
+    public async Task<string> Post(Location location, CancellationToken token)
+    {
+        _logger.Log(LogLevel.Information, new EventId, null, "Location name sent" + location.Name, null);
+
+        var x = await _service.GetNearestBodyOfWater(location, token);
+        return $"The nearest body of water to {location.Name} is {x}";
+    }
+        
+   }
+
+
