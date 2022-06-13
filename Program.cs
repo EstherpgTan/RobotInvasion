8f7c25eb85ea25e9d3f6173ace6b35346b45d034
Esther Tan <EstherpgTan@gmail.com>
2022-06-13 13:33:06 +1000

    Latest update

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using RobotInvasion2.Services;
 using RobotInvasion2.Model;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -11,8 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<LocationContext>(options => options.UseInMemoryDatabase("LocalDB"));
-
+// IOC container
 builder.Services.AddSingleton<HttpClient>();
 builder.Services.AddSingleton<LocationService>();
 
